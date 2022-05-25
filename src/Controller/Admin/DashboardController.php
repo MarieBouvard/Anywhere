ac5dd23af54d55a4efc7f90b0414e20baac3b6b6
mariebouvard <mariebouvard@outlook.com>
2022-05-25 16:44:57 +0200

    Update of Admin Dashboard controller - add of User crud controller

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Category;
 use App\Entity\Travel;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -29,8 +30,9 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('User', 'fa fa-user', User::class);
         yield MenuItem::linkToCrud('Catégorie', 'fa fa-list', Category::class);
-        yield MenuItem::linkToCrud('Voyages', 'fa fa-travel', Travel::class);
+        yield MenuItem::linkToCrud('Voyages', 'fa fa-plane', Travel::class);
         
     }
 }
